f238ef9ee3c19b7a029beb9591c6640a8951164d
Gustav Andersson <gustav87and@gmail.com>
2022-02-22 23:29:55 +0100

    Fix MathJax rendering in github pages 2

--- assets/js/mathjax-config.js
+++ assets/js/mathjax-config.js
@@ -2,10 +2,6 @@ var MathJax = {
   jax: ["input/TeX", "output/HTML-CSS"],
   tex2jax: {
     inlineMath: [['$', '$'],['\\(','\\)']],
-    displayMath: [ ['$$', '$$'] ],
-    processEscapes: true,
-    skipTags: ['script', 'noscript', 'style', 'textarea', 'pre', 'code']
-  },
-  messageStyle: "none",
-  "HTML-CSS": { preferredFont: "TeX", availableFonts: ["STIX","TeX"] }
+    processEscapes: true
+  }
 }
